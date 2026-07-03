0c38aa89c3b5f2a8b07cc5d8ae1c2729ca99fc4e
Igor Kostyuchenok <igor.kostyuchenok@gmail.com>
2026-07-03 11:25:17 +0200

    motion is not defined

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import profilePic from "./assets/me.png";
 import { books } from "./data/books";
 import locationData from "./data/location.json";
